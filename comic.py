f52048abedcbeac8892684b9e5af59e617f6e2f3
Lunch <Lunch650@qq.com>
2018-01-12 00:19:24 +0800

    一个开始

--- comic.py
+++ comic.py
@@ -3,9 +3,11 @@
 
 class Comic(object):
     root = 'http://www.mhkkm.la/riben/wuyiniao/'
-    def __init__(self):
-        self.comic_id = ''
+    def __init__(self, id):
+        self.comic_id = id
         self.comic_url = Comic.root + self.comic_id + '.html'
+        self.comic_num_pages = -1
+        self.comic_title = ''
 
     def content_comic_index(self):
         pass
