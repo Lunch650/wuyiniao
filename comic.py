689ed0f64b5c2aad166fd9c7f0e56ea4eafdb57a
lunch650 <lunch650@qq.com>
2018-01-17 17:40:25 +0800

    简单更新

--- comic.py
+++ comic.py
@@ -1,13 +1,29 @@
 #!python3
 # coding=utf8
+import requests
+from bs4 import BeautifulSoup
+
 
 class Comic(object):
     root = 'http://www.mhkkm.la/riben/wuyiniao/'
-    def __init__(self, id):
-        self.comic_id = id
-        self.comic_url = Comic.root + self.comic_id + '.html'
-        self.comic_num_pages = -1
-        self.comic_title = ''
 
-    def content_comic_index(self):
-        pass
+    def __init__(self, comic_id):
+        self.id = comic_id
+        self.url = Comic.root + self.id + '.html'
+        self.num_pages = int(Comic.content_comic_index(self.url).find('ul', class_='pagelist').li.text[1:-3])
+        self.title = Comic.content_comic_index(self.url).find('h1').text
+
+    @staticmethod
+    def content_comic_index(url):
+        r = None
+        while r is None:
+            try:
+                r = requests.get(url, timeout=5)
+            except requests.Timeout:
+                print("Timeout,retry")
+        return BeautifulSoup(r.content, 'lxml')
+
+
+if __name__ == '__main__':
+    c = Comic('7390')
+    print(c.num_pages)
