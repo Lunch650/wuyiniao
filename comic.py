f87d2b218506c3b734fb4c9767bf945fc19dea2b
lunch650 <lunch650@qq.com>
2018-01-24 12:10:35 +0800

    201801241210

--- comic.py
+++ comic.py
@@ -1,6 +1,7 @@
 #!python3
 # coding=utf8
 import requests
+import os
 from bs4 import BeautifulSoup
 
 
@@ -15,23 +16,35 @@ class Comic(object):
         self.comic_title = self.title()
 
     def num_pages(self):
-        #返回漫画页数
+        # 返回漫画页数
         return int(self.comic_index_content.find('ul', class_='pagelist').li.text[1:-3])
 
     def title(self):
-        #返回漫画标题
+        # 返回漫画标题
         return self.comic_index_content.find('h1').text
 
-    def pic_pages(self):
-        #返回漫画各页地址，返回的数据格式为List
+    def pages(self):
+        # 返回漫画各页地址，返回的数据格式为List
         pic_pages = [self.comic_url]
         for num in range(2, self.comic_num_pages + 1):
             pic_pages.append(self.comic_url[:-5] + '_' + str(num) + '.html')
         return pic_pages
 
+    def mkdir(self):
+        # 创建文件夹
+        root_path = 'E:\\Lunch\\Comics\\wuyiniao\\'
+        if not os.path.exists(root_path + self.comic_id):
+            try:
+                os.mkdir(root_path + self.comic_id)
+            except OSError as e:
+                print('创建文件夹失败,原因:', e)
+        else:
+            print(self.comic_id + '的文件夹已存在')
+
+
     @staticmethod
     def pic_from_page(url):
-        #返回页面中的图片地址
+        # 返回页面中的图片地址
         return Comic.page_soup(url).select('span#t_right')[0].next_sibling.next_sibling['src']
 
     @staticmethod
@@ -49,5 +62,8 @@ if __name__ == '__main__':
     c = Comic('7391')
     print(c.comic_num_pages)
     print(c.comic_title)
-    for pic_page in c.pic_pages():
-        print(Comic.pic_from_page(pic_page))
+    pic_content = requests.get(Comic.pic_from_page(c.pages()[0]), stream=True)
+    with open('E:\\Lunch\\Comics\\wuyiniao\\' + c.comic_id + '1.jpg', 'wb') as pic_file:
+        for p in pic_content:
+            print(p)
+
